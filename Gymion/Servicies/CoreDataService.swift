72d4d4559eee116b19e40c629db9b08d648c608b
Eugen <belexiu.eugen@icloud.com>
2026-02-17 08:53:59 +0200

    feat: New exercise controller and modal presentation

---
+++ Gymion/Servicies/CoreDataService.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataService.swift
+//  Gymion
+//
+//  Created by Belexiu Eugeniu on 16.02.2026.
+//
+
+import Foundation
---
+++ Gymion/Views/Exercise/CreateExcersiseVC.swift
@@ -0,0 +1,29 @@
+//
+//  createExcersiseVC.swift
+//  Gymion
+//
+//  Created by Belexiu Eugeniu on 16.02.2026.
+//
+
+import UIKit
+
+class createExcersiseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
